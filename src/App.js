ac0b953a93680f2b529d02c74dc60c9796f76815
Stjepan <dzojicsjepan1812@gmail.com>
2021-02-02 23:56:52 +0100

    Made a PokemonList that renders 2 pokemons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import PokemonList from './PokemonList';
+import PokemList from './PokemonList'
 function App() {
+  const [pokemon, setPokemon] = useState(["bulbasaur","charmander"])
+
   return (
-    null
+    <PokemonList pokemon={pokemon}/>
   );
 }
 
